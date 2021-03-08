56c0850be8aa6eed840d1e5b962b7e0089706da6
TuoBa <liweieternity@163.com>
2021-03-08 16:54:54 +0800

    add some issue description about days_between.py.

--- checkio/days_between.py
+++ checkio/days_between.py
@@ -1,5 +1,17 @@
 from datetime import date, timedelta
 
+'''
+    How old are you in a number of days? It's easy to calculate - just subtract your birthday from today. We could make this a real challenge though and count the difference between any dates.
+
+    You are given two dates as an array with three numbers - a year, month and day. For example: 19 April 1982 will be (1982, 4, 19). You should find the difference in days between the given dates. For example between today and tomorrow = 1 day. The difference will always be either a positive number or zero, so don't forget about the absolute value.
+
+    Input: Two dates as tuples of integers.
+
+    Output: The difference between the dates in days as an integer.
+
+    Precondition: Dates between 1 january 1 and 31 december 9999. Dates are correct.    
+'''
+
 def days_diff(a, b):
     # your code here
     yeara, montha, daya = a
