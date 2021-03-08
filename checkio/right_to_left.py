5c6cc110d89e5a6a67e65bc293bf4e5ea09c8734
TuoBa <liweieternity@163.com>
2021-03-08 15:12:43 +0800

    add mission info in right_to_left.py

--- checkio/right_to_left.py
+++ checkio/right_to_left.py
@@ -2,6 +2,18 @@ def left_join(phrases):
     """
         Join strings and replace "right" to "left"
     """
+    """
+    One of the robots is charged with a simple task: to join a sequence of strings into one sentence to produce instructions on how to get around the ship. But this robot is left-handed and has a tendency to joke around and confuse its right-handed friends.
+
+    You are given a sequence of strings. You should join these strings into a chunk of text where the initial strings are separated by commas. As a joke on the right handed robots, you should replace all cases of the words "right" with the word "left", even if it's a part of another word. All strings are given in lowercase.
+
+    Input: A sequence of strings.
+
+    Output: The text as a comma-separated string.
+
+    Precondition:
+    0 < len(phrases) < 42
+    """
     return ",".join([phrase.replace("right", "left") for phrase in phrases])
     
 
